030235a243adda7d027e7c4ccb5b8d560e1b1748
Aytac11 <aytac11011@gmail.com>
2021-03-02 20:09:38 +0400

    update task12

--- week6/day1.2/task12.js
+++ week6/day1.2/task12.js
@@ -1,31 +1,41 @@
 // Ad gününüzə neçə gün qalıb? bunu tapmaq üçün proqram yazmaq (JS Date)
 
-const date = new Date();
+// --------------------method 1----------------------
 
-const birthday = 3;
-const birthmonth = 3;
-const birthyear = 2021;
+// const date = new Date();
 
-const currentDay = date.getDate();
-const currentMonth = date.getMonth()+1;
-const currentYear = date.getFullYear();
+// const birthday = 3;
+// const birthmonth = 3;
+// const birthyear = 2021;
+
+// const currentDay = date.getDate();
+// const currentMonth = date.getMonth()+1;
+// const currentYear = date.getFullYear();
 
 
-let resultday = (currentDay-birthday);
-if(resultday<0){
-    resultday = Math.abs(resultday);
-} 
+// let resultday = (currentDay-birthday);
+// if(resultday<0){
+//     resultday = Math.abs(resultday);
+// } 
 
-let resultmonth = currentMonth-birthmonth;
-if(resultmonth<0){
-    resultmonth = Math.abs(resultmonth);
-} 
+// let resultmonth = currentMonth-birthmonth;
+// if(resultmonth<0){
+//     resultmonth = Math.abs(resultmonth);
+// } 
 
-let resultyear = currentYear-birthyear;
-if(resultyear<0){
-    resultyear= Math.abs(resultyear);
-} 
+// let resultyear = currentYear-birthyear;
+// if(resultyear<0){
+//     resultyear= Math.abs(resultyear);
+// } 
 
-let result =resultyear*365-(resultday + resultmonth*31);
+// let result =resultyear*365-(resultday + resultmonth*31);
 
-console.log(result)
+// console.log(result)
+
+// ---------------method 2---------------
+
+const d = new Date(); // hazirki tarix
+console.log(d.getHours());
+
+const nextBirthday = new Date(2000, 1, 12); // custom tarix yarat
+console.log(nextBirthday);
