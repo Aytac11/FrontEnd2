6e4e9c6ec4da2705a09ad8b895129bead0a38a91
Aytac11 <aytac11011@gmail.com>
2021-03-05 22:45:20 +0400

    update3

--- week7/day3/task16.js
+++ week7/day3/task16.js
@@ -11,19 +11,3 @@ const find = (num1) =>{
 }
 
 console.log(find(10));
-
-
-
-// let num1 = 0;
-// let num2 = 1;
-// let c;
-// let s = "";
-
-// for(let i=0; i < 10; i++){
-//     c = num1 + num2;
-//     num1 = num2;
-//     num2 = c;
-//     s += num1 + " ";
-// }
-
-// console.log("0",s);
