5174445f9946814e60efdbb6071856d639f2c5ff
Aytac11 <aytac11011@gmail.com>
2021-03-30 16:49:38 +0400

    create accordion

---
+++ week10/day1/js/accordian.js
@@ -0,0 +1,17 @@
+let accordion = document.querySelectorAll('.accordion');
+
+for (let i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle("active");
+        let text = this.nextElementSibling;
+        if (text.style.display == "block" && this.children[0].classList.contains('fa-chevron-up')) {
+            text.style.display = "none";
+            this.children[0].classList.remove('fa-chevron-up');
+            this.children[0].classList.add('fa-chevron-down');
+        } else {
+            text.style.display = "block";
+            this.children[0].classList.remove('fa-chevron-down');
+            this.children[0].classList.add('fa-chevron-up');
+        }
+    })
+}
