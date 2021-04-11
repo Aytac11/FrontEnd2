99c74d9ac8638762367f95ea1a1260238530633b
Aytac11 <aytac11011@gmail.com>
2021-04-11 17:03:18 +0400

    dice roller completed

--- week11/diceRoller/js/app.js
+++ week11/diceRoller/js/app.js
@@ -17,23 +17,40 @@
       [0, 2, 3, 5, 6, 8],
   ];
 
-  const dots = document.querySelectorAll('.dot');
-  dots[1].style.opacity = '0';
-  dots[4].style.opacity = '0';
-  dots[7].style.opacity = '0';
+  const dots1 = document.querySelectorAll('.dot');
+  const dots2 = document.querySelectorAll('.dot2');
+
+  dots1[1].style.opacity = '0';
+  dots1[4].style.opacity = '0';
+  dots1[7].style.opacity = '0';
+
+  dots2[1].style.opacity = '0';
+  dots2[4].style.opacity = '0';
+  dots2[7].style.opacity = '0';
 
   addEvent('button', 'click', () => {
-      for (let j = 0; j < 2; j++) {
-          const rand = random(0, 5);
-          console.log(rand + 1);
+      const rand = random(0, 5);
+      const rand2 = random(0, 5);
+      //console.log(rand2 + 1);
 
-          for (let i = 0; i < dots.length; i++) {
-              dots[i].style.opacity = '0';
-          }
-
-          for (let i = 0; i < dices[rand].length; i++) {
-              dots[dices[rand][i]].style.opacity = '1';
-          }
-
+      for (let i = 0; i < dots1.length; i++) {
+          dots1[i].style.opacity = '0';
       }
+
+      for (let i = 0; i < dices[rand].length; i++) {
+          dots1[dices[rand][i]].style.opacity = '1';
+      }
+      for (let j = 0; j < dots2.length; j++) {
+          dots2[j].style.opacity = '0';
+          //console.log(dots2[j])
+      }
+
+      for (let j = 0; j < dices[rand2].length; j++) {
+          dots2[dices[rand2][j]].style.opacity = '1';
+      }
+
+      var score = document.querySelector('.score');
+      let sum = rand + rand2 + 2;
+      score.innerText = sum;
+
   });
