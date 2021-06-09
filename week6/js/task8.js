87ae1f968d2dff49293c7084ed6a67084615726e
Aytac11 <aytac11011@gmail.com>
2021-06-09 12:14:53 +0400

    add task8

--- week6/js/task8.js
+++ week6/js/task8.js
@@ -6,8 +6,20 @@ let sait = ['a', 'i', 'o', 'u', 'e'];
 
 let newArr = [];
 for (let i = 0; i < arr.length; i++) {
-    if (typeof(arr[i]) === String) {
+    if (typeof(arr[i]) === 'string') {
         newArr += arr[i];
     }
 }
-console.log(newArr);
+
+let count = 0;
+for (let k = 0; k < newArr.length; k++) {
+    for (let j = 0; j < sait.length; j++) {
+        if (newArr[k] === sait[j] || newArr[k] === sait[j].toUpperCase()) {
+            count++;
+        }
+    }
+}
+let samit = newArr.length - count;
+
+console.log("Saitlerin sayi:" + count);
+console.log("Samitlerin sayi:" + samit);
