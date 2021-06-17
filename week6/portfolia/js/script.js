03f48562fdac705f13aee853f12d2a2114f84fd3
Aytac11 <aytac11011@gmail.com>
2021-06-17 14:25:06 +0400

    add blog section

--- week6/portfolia/js/script.js
+++ week6/portfolia/js/script.js
@@ -14,3 +14,33 @@ closeMenu.addEventListener('click', function() {
     openMenu.style.opacity = "100%"
     logo.style.opacity = "100%";
 });
+
+$(document).ready(function() {
+
+    var headerHeight = $('header').outerHeight(); // Target your header navigation here
+
+    $('#main-nav li a').click(function(e) {
+
+        var targetHref = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(targetHref).offset().top - headerHeight // Add it to the calculation here
+        }, 1000);
+
+        e.preventDefault();
+    });
+});
+
+$('.content-wrapper').scrollSpy({
+    target: $('.menu a')
+}).scroll();
+
+$('.content-wrapper').scrollSpy({
+    target: $('.menu a'),
+    activeClass: 'active'
+}).scroll();
+
+$('.content-wrapper').scrollSpy({
+    target: $('.menu a'),
+    fixed: true
+}).scroll();
