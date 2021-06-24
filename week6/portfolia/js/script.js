27bb431db6ff469fbf7de57597c14c454d58dce2
Aytac11 <aytac11011@gmail.com>
2021-06-24 15:34:32 +0400

    corrected task3

--- week6/portfolia/js/script.js
+++ week6/portfolia/js/script.js
@@ -31,16 +31,29 @@ $(document).ready(function() {
     });
 });
 
-$('.content-wrapper').scrollSpy({
-    target: $('.menu a')
-}).scroll();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-$('.content-wrapper').scrollSpy({
-    target: $('.menu a'),
-    activeClass: 'active'
-}).scroll();
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
 
-$('.content-wrapper').scrollSpy({
-    target: $('.menu a'),
-    fixed: true
-}).scroll();
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
--- week7/js/task3.js
+++ week7/js/task3.js
@@ -3,13 +3,15 @@ const find = (arr) => {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
         let s = 0;
+        let b;
+        b = arr[i];
         while (arr[i] > 0) {
             let remainder = arr[i] % 10;
             arr[i] = Math.floor(arr[i] / 10);
             s += remainder;
         }
         if (s < 10) {
-            newArr.push(s);
+            newArr.push(b);
 
         }
     }
